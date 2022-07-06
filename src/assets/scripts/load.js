2435338d57aafb4b9f8d03b3067695dc55e47476
Céo <noah.kd@gmail.com>
2022-07-06 20:48:49 -0300

    OUFGGOUYGU

--- src/assets/scripts/load.js
+++ src/assets/scripts/load.js
@@ -4,30 +4,24 @@ window.addEventListener( "load", () => {
 
     var drawerSwitch = document.querySelector( "appbar-left" );
     var drawerToggle = document.querySelector( "appbar-right" );
-    var option = document.querySelectorAll( "option" );
+    var options = document.querySelectorAll( "option" );
     var drawer = document.querySelector( "drawer" );
     var drawerObserver = 0;
 
-    function print( v ) { console.table( v ); }
+    function print( v ) { console.log( v ); }
     drawer.style.width = "0";
 
     
     drawerSwitch.addEventListener( "click", () => {
         switch( drawer.style.width ) {
             case "": drawer.style.width = "350px";
-                     option[0].style.display = "block";
-                     option[1].style.display = "block";
-                     option[2].style.display = "block";
+                     options.forEach( el => el.style.display = "block" );
                 break;
             case "68px": drawer.style.width = "350px";
-                         option[0].style.display = "block";
-                         option[1].style.display = "block";
-                         option[2].style.display = "block";
+                         options.forEach( el => el.style.display = "block" );
                 break;
             case "350px": drawer.style.width = "68px";
-                          option[0].style.display = "none";
-                          option[1].style.display = "none";
-                          option[2].style.display = "none";
+                          options.forEach( el => el.style.display = "none" );
                 break;
         }
     } );
@@ -36,24 +30,15 @@ window.addEventListener( "load", () => {
         switch( drawerObserver ) {
             case 0: drawer.style.width = "68px";
                     drawerObserver = 2;
-                    option[0].style.display = "none";
-                    option[1].style.display = "none";
-                    option[2].style.display = "none";
-                    print( `drawerToggle: ${this} - observer: 0` );
+                    options.forEach( el => el.style.display = "none" );
                 break;
             case 1: drawer.style.width = "68px";
                     drawerObserver = 2;
-                    option[0].style.display = "none";
-                    option[1].style.display = "none";
-                    option[2].style.display = "none";
-                    print( `drawerToggle: ${this} - observer: 1` );
+                    options.forEach( el => el.style.display = "none" );
                 break;
             case 2: drawer.style.width = "0";
                     drawerObserver = 1;
-                    option[0].style.display = "none";
-                    option[1].style.display = "none";
-                    option[2].style.display = "none";
-                    print( `drawerToggle: ${this} - observer: 2` );
+                    options.forEach( el => el.style.display = "none" );
                 break;
         }
     } );
